2a7d7aef22190843606db4b2c8708e6e82a20ce5
Alexander Khrichikov <alex-ander-97@mail.ru>
2025-02-09 16:37:01 +0500

    Добавил новые файлы

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ app = FastAPI()
 def index():
     return {'title' : 'index',
             'program_versiobn': '0.0.1'}
+
+@app.get('/about_us')
+def about_us():
+    return {
+        'title': 'About us',
+        'text': 'I am Alex.',
+        'mail': 'A@aa.a'
+    }
